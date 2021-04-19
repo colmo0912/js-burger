5f6fbab6778c6f78fc2963649328131d7ef37e7a
colmo0912 <colmo0912@gmail.com>
2021-04-19 16:32:18 +0200

    aggiungo cariabili burger e btn

---
+++ js/liveScript.js
@@ -0,0 +1,28 @@
+
+//prendo l'input dove inserire il nome del burger
+
+var nomeBurger = document.getElementById("name");
+console.log(nomeBurger.value)
+
+var btnCalculate  = document.getElementById("button")
+
+btnCalculate.addEventListener("click" , function(event){
+    
+
+if (nomeBurger = " "){
+
+    alert("devi scrivere il nome del tuo panino")
+}
+
+
+var cheese = document.getElementById("cheese-add")
+console.log(cheese.value)
+
+
+
+
+
+})
+
+
+
