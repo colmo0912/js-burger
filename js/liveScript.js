534a5e4a4628f5e81eb3db57559d6e1292d8dbd2
colmo0912 <colmo0912@gmail.com>
2021-04-20 14:20:27 +0200

    da completare con il codice sconto

--- js/liveScript.js
+++ js/liveScript.js
@@ -4,13 +4,11 @@ var btnCalculate  = document.getElementById("button")
 
 btnCalculate.addEventListener("click" , function(){
     
-
-
     var nomeBurger = document.getElementById("name");
     // if (nomeBurger = " "){
 
-//     alert("devi scrivere il nome del tuo panino")
-// }
+    //     alert("devi scrivere il nome del tuo panino")
+    // }
 
     var ingredienti = document.getElementsByClassName("ingredient-checkbox")
     console.log(ingredienti)
@@ -26,36 +24,39 @@ btnCalculate.addEventListener("click" , function(){
         }
     }
 
-    
-    var costoTotale = document.getElementById("price").innerHTML = somma
+    document.getElementById("price").innerHTML = somma
 
-    var codiceUno = "ANDREA01"
-    var codiceDue = "ANDREA02"
-    var codiceTre = "ANDREA03"
     var coupon = document.getElementById("coupon")
 
-    // var codiciSconti =["ANDREA01", "ANDREA02" , "ANDREA03"] 
+    var codiciSconti =["ANDREA01", "ANDREA02" , "ANDREA03"] 
 
-    // for (var i = 0; i<codiciSconti.length; i++){
+    for (var i = 0; i < codiciSconti.length; i++){
 
-    //     var sconto = codiciSconti[i];
+        var sconto = codiciSconti[i];
 
-        if (coupon.value = codiceUno )
+        if (coupon.value = sconto )
         {
-    
-            somma -= ((somma*20)/100)
-        }else if (coupon.value= codiceDue){
-    
-            somma -= ((somma*30)/100)
-        }else if (coupon.value= codiceTre){
-    
-            somma -= ((somma*50)/100)
-        }
 
-        document.getElementById("price").innerHTML = somma
-    
-    
+        somma -= ((somma*20)/100)}
+            // }else if (coupon.value= sconto){
+
+            //     somma -= ((somma*30)/100)
+            // }else if (coupon.value= sconto){
+
+            //     somma -= ((somma*50)/100)
+            // }
+    }    
+
+        document.getElementById("price").innerHTML = somma.toFixed(2)
 
 })
+
+
+
+
+
+
+
+
 
 
